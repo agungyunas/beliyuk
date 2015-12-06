5da4faffa20543b4536088186b0a663d8f705313
agungyunas <agungyunass@gmail.com>
2015-12-07 00:47:37 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,19 +1,19 @@
 <?php
 //host yang digunakan
 //99,9% tidak perlu dirubah
-$host = 'localhost'; 
+$host = 'br-cdbr-azure-south-a.cloudapp.net'; 
  
 //username untuk login ke host
 //biasanya didapatkan pada email konfirmasi order hosting
-$user = 'root'; 
+$user = 'b5cfd164543a2e'; 
  
 //jika menggunakan PC sendiri sebagai host,
 //secara default password dikosongkan
-$pass = '';
+$pass = '6af6daa5';
  
 //isikan nama database sesuai database
 //yang dibuat pada langkah-1
-$dbname = 'web';
+$dbname = 'acsm_6ab719fe97915dd';
  
 //mengubung ke host
 $connect = mysql_connect($host, $user, $pass) or die(mysql_error());
