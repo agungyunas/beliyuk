a3a9d20c2764ca6de530c030d014023b4e869e77
agungyunas <agungyunass@gmail.com>
2015-12-07 22:01:29 +0700

    Update proses_tambahhak.php

--- Admin/proses_tambahhak.php
+++ Admin/proses_tambahhak.php
@@ -1,32 +1,16 @@
 <?php session_start(); ?>
 		<?php
 			include('koneksi.php');
-				
-			$query = "select * from akun_user where username='".$_POST['username']."'";
-			
-			$hasil = mysql_query($query) or die ($mysql_error());
-			
-			$hitung = mysql_num_rows($hasil);
-			
-			if($hitung == 0){
 			
 					$query = "insert into user_admin (username, password, status) value ('".$_POST['username']."','".$_POST['password']."','admin')";
 						
-						$hasil=mysql_query($query) or die ($mysql_error());
+						$hasil=mysql_query($query) or die (mysql_error());
 						
 						if($hasil){
-							echo "<script> alert('register berhasil selamat !!'); window.location = 'haladmin.php'; </script>";
+							echo "<script> alert('Anda berhasil Menambah Hak akses admin!!'); window.location = 'haladmin.php'; </script>";
 						}
 						else {
-							echo "<script> alert('register gagal!!'); window.history.back(); </script>";
+							echo "<script> alert('Anda gagal konfirmasi!!'); window.history.back(); </script>";
 						}
-						
-				}
-				
-			}
-			else  
-			{
-				echo "<script> alert('Username telah digunakan'); window.history.back(); </script>";
-			}
 			
 			?>
