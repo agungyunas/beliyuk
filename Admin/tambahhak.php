125bb79c0cc0026d62ebfbb1f0febf7d6fa6ea7d
agungyunas <agungyunass@gmail.com>
2015-12-07 21:23:32 +0700

    Create tambahhak.php

---
+++ Admin/tambahhak.php
@@ -0,0 +1,51 @@
+
+				
+	<table bORDER="0" align="center">	
+			<form action="proses_daftar.php" method="post" name="daftar"><br>	
+								<tr>
+									<td colspan="3" align="center">
+								
+								<font size="5" face="aharoni" color="green"> DAFTAR </font><br><br>
+									
+									</td>
+								</tr>		
+								
+								<tr>
+									<td align="left"> 
+									<label> Username</label>
+										</td>
+										
+										<td width="10">:</td>
+									<td>
+										<input type="text" name="username" placeholder=" Username" size="50"><br>
+									</td>
+								
+								</tr>
+								
+								<tr>
+									<td align="left"> 
+									<label> Password</label>
+										</td>
+										
+										<td width="10">:</td>
+									<td>
+										<input type="password" name="password" placeholder=" Password " size="50"><br>
+									</td>
+								
+								</tr>
+								
+								<tr>
+									<td align="left"> 
+									&nbsp;
+									</td>
+									<td width="10">&nbsp;</td>
+									<td>
+								 <input type="reset" value="Reset" name="reset">
+									<input type="submit" value="Simpan" name="pesan">
+									</td>
+								</tr>
+								
+								
+								
+					<form>
+					</table>
